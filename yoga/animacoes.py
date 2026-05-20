39b3d85736799fa4621d99228b7fb0d5299d9cc4
m41k <maik.alberto@hotmail.com>
2026-05-20 17:07:31 -0300

    Add files via upload

---
+++ yoga/animacoes.py
@@ -0,0 +1,45 @@
+from .sprites import *
+
+estatico_frames = [
+    (a1, 50, 50),
+]
+
+bufando_frames = [
+    (a1, 50, 50),
+    (a2, 50, 50),
+    (a3, 50, 50),
+    (a4, 50, 50),
+    (a5, 50, 50),
+    (a6, 50, 50),
+]
+
+olhando_frames = [
+    (b1, 50, 50),
+    (b2, 50, 50),
+    (b3, 50, 50),
+    (b4, 50, 50),
+    (b5, 50, 50),
+    (b6, 50, 50),
+]
+
+button_frames = [
+    (c1, 50, 50),
+    (c2, 50, 50),
+    (c3, 50, 50),
+    (c4, 50, 50),
+    (c5, 50, 50),
+    (c6, 50, 50),
+    (d1, 50, 50),
+    (d2, 50, 50),
+    (d3, 50, 50),
+    (d4, 50, 50),
+    (d5, 50, 50),
+    (d6, 50, 50),
+]
+
+animacoes = {
+    "estatico": estatico_frames,
+    "bufando": bufando_frames,
+    "olhando": olhando_frames,
+    "botao": button_frames,
+}
---
+++ yoga/main.py
@@ -0,0 +1,20 @@
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+import framebuf
+import time
+
+# =========================================================
+# OLED
+# =========================================================
+
+i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
+oled = SSD1306_I2C(128, 64, i2c)
+
+
+# mostra logo
+oled.fill(0)
+oled.text("M41k", 45, 30)
+oled.show()
+
+
+from . import frame
