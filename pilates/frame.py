9e50ee29c6ed833e30403990420541d8db80508c
m41k <maik.alberto@hotmail.com>
2026-05-28 10:55:03 -0300

    Create frame.py

---
+++ pilates/frame.py
@@ -0,0 +1,67 @@
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+import framebuf
+import time
+
+from .animacoes import animacoes
+
+# =========================================================
+# OLED
+# =========================================================
+
+i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
+oled = SSD1306_I2C(128, 64, i2c)
+
+# =========================================================
+# FRAMEBUFFER
+# =========================================================
+
+def criar_frame(data, largura, altura):
+
+    return framebuf.FrameBuffer(
+        bytearray(data),
+        largura,
+        altura,
+        framebuf.MONO_HLSB
+    )
+
+# =========================================================
+# MOSTRAR
+# =========================================================
+
+def mostrar(data, w, h):
+
+    frame = criar_frame(data, w, h)
+
+    x = (128 - w) // 2
+    y = (64 - h) // 2
+
+    oled.fill(1)
+
+    oled.blit(frame, x, y)
+
+    oled.show()
+
+# =========================================================
+# LOOP
+# =========================================================
+
+irritado = 0
+
+while True:
+
+    # =====================================================
+    # ESCOLHER ANIMAÇÃO
+    # =====================================================
+
+        animacao = "hundred"
+
+    # =====================================================
+    # TOCAR ANIMAÇÃO
+    # =====================================================
+
+    for data, w, h in animacoes[animacao]:
+
+        mostrar(data, w, h)
+
+        time.sleep_ms(120)
