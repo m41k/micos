96aef7756edafe7244536f81becde3e89dd61e40
m41k <maik.alberto@hotmail.com>
2026-05-28 10:33:32 -0300

    Create animacoes.py

---
+++ pilates/animacoes.py
@@ -0,0 +1,13 @@
+from .sprites import *
+
+hundred_frames = [
+    (a1, 32, 20),
+    (a2, 36, 20),
+    (a3, 45, 20),
+    (a4, 40, 20),
+    (a5, 29, 20),
+]
+
+animacoes = {
+    "hundred": hundred_frames,
+}
